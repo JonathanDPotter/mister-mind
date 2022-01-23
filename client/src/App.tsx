98c32b5abd7c3ff844f4cd5a266b2b4608a22255
Jonathan <jonathan.d.potter@outlook.com>
2022-01-23 16:39:14 -0500

    trying fade groups

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 // components
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/pages/Home";
 import Register from "./components/pages/Register";
 
+const FadeRoutes = () => {
+  const location = useLocation();
+  return (
+    <TransitionGroup key={location.key}>
+      <CSSTransition timeout={3000} classNames="fade">
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/logout" element={<h1>Logout</h1>} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/game" element={<h1>Game</h1>} />
+          <Route path="/scores" element={<h1>Scores</h1>} />
+          <Route path="/leader" element={<h1>Leader</h1>} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
 function App() {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/logout" element={<h1>Logout</h1>} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/game" element={<h1>Game</h1>} />
-        <Route path="/scores" element={<h1>Scores</h1>} />
-        <Route path="/leader" element={<h1>Leader</h1>} />
-      </Routes>
+      <FadeRoutes />
     </Router>
   );
 }

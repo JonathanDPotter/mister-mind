c8926b38c89b9a1c6359674fe7a9677fa7b67450
Jonathan <jonathan.d.potter@outlook.com>
2022-01-24 07:38:54 -0500

    page transitions with framer-motion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,29 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 // components
+import Framer from "./components/Framer/Framer";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/pages/Home";
 import Register from "./components/pages/Register";
 
-const FadeRoutes = () => {
+const App = () => {
   const location = useLocation();
   return (
-    <TransitionGroup key={location.key}>
-      <CSSTransition timeout={3000} classNames="fade">
-        <Routes location={location}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<h1>Login</h1>} />
-          <Route path="/logout" element={<h1>Logout</h1>} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/game" element={<h1>Game</h1>} />
-          <Route path="/scores" element={<h1>Scores</h1>} />
-          <Route path="/leader" element={<h1>Leader</h1>} />
+    <>
+      <NavBar />
+      <AnimatePresence exitBeforeEnter={true}>
+        <Routes location={location} key={location.key}>
+          <Route path="/" element={<Framer Component={<Home />} />} />
+          <Route
+            path="/Register"
+            element={<Framer Component={<Register />} />}
+          />
+          <Route path="*" element={<Framer Component={<h1>Not Found</h1>} />} />
         </Routes>
-      </CSSTransition>
-    </TransitionGroup>
+      </AnimatePresence>
+    </>
   );
 };
-
-function App() {
-  return (
-    <Router>
-      <NavBar />
-      <FadeRoutes />
-    </Router>
-  );
-}
 
 export default App;
